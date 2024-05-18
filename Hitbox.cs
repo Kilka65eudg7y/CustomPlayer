ab6a30d0d0c526890d8106c28f759528848d6552
White_Sky <107893681+WhiteSkyDev@users.noreply.github.com>
2024-05-18 12:36:48 +0300

    Update Hitbox.cs

--- Hitbox.cs
+++ Hitbox.cs
@@ -3,10 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
 public class Hitbox : MonoBehaviour, IDamageable
 {
     [HideInInspector] public BaseEntity Parent;
     [SerializeField] private float DamageMultiplier;
+    private Collider HitboxCollider;
+
     public string HitboxName
     {
         get
@@ -21,5 +25,10 @@ public class Hitbox : MonoBehaviour, IDamageable
     public void TakeDamage(int Damage)
     {
         Parent.TakeDamage((int)((float)Damage * DamageMultiplier));
+    }
+    private void Start()
+    {
+        HitboxCollider = GetComponent<Collider>();
+        HitboxCollider.isTrigger = true;
     }
 }
